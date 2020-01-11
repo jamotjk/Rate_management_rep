ffdbd2602d0b83272f9ae14c07ccf8d54a194adf
jamotjk <52899558+jamotjk@users.noreply.github.com>
2020-01-12 00:00:19 +0800

    coomen

--- db.connect.php
+++ db.connect.php
@@ -1,4 +1,14 @@
 <?php 
-$conn=mysqli_connect("db4free.net:3306","jamotjk","11jamot");
-mysqli_select_db($conn,"rate_database");
+
+//$conn=mysqli_connect(":3306","jamotjk","11jamot");
+//mysqli_select_db($conn,"rate_database");
+
+
+
+$conn = new mysqli("db4free.net", "jamotjk", "11jamot11_", "rate_database", 3306);
+if ($coon->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+}
+
+echo $coon->host_info . "\n";
  ?>
