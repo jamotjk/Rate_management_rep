2df70a0a06b0cb66eeb7d0406265c829582bcd82
jamotjk <52899558+jamotjk@users.noreply.github.com>
2020-01-12 00:12:06 +0800

    new

--- db.connect.php
+++ db.connect.php
@@ -3,12 +3,9 @@
 //$conn=mysqli_connect(":3306","jamotjk","11jamot");
 //mysqli_select_db($conn,"rate_database");
 
+$conn = mysqli_connect("db4free.net:3306", "jamotjk", "11jamot_11_", "rate_database");
 
 
-$conn = new mysqli("db4free.net", "jamotjk", "11jamot11_", "rate_database", 3306);
-if ($conn->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
-}
 
-echo $conn->host_info . "\n";
+
  ?>
