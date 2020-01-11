0b24d562968b062f8c99c2917edd9a6395449ffd
jamotjk <52899558+jamotjk@users.noreply.github.com>
2020-01-12 00:23:09 +0800

    com

--- db.connect.php
+++ db.connect.php
@@ -3,9 +3,18 @@
 //$conn=mysqli_connect(":3306","jamotjk","11jamot");
 //mysqli_select_db($conn,"rate_database");
 
-$conn = mysqli_connect("db4free.net:3306", "jamotjk", "11jamot_11_", "rate_database");
 
+$servername = "db4free.net:3306";
+$username = "jamotjk";
+$password = "11jamot_11_";
+$dbname="rate_database";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
 
-
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 
  ?>
